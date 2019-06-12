503096d1a79223eb143b0fe01f7cbc3211371a7d
cban1980 <cban@gmx.com>
2019-06-12 15:27:03 +0200

    minor bug 2

--- bot.py
+++ bot.py
@@ -53,6 +53,7 @@ async def on_message(message):
         commands['!why']='Randomiserad BOFH reason.'
         msg=discord.Embed(title='𝐃𝐢𝐬𝐜𝐨𝐫𝐝𝐛𝐨𝐭𝐞𝐧 𝐁𝐞𝐧𝐠𝐭', color=0x008000)
         for command,description in commands.items():
+            msg.set_thumbnail(url="http://www.2pnews.com/wp-content/uploads/2013/04/hans-freekok.jpg")
             msg.add_field(name=command,value=htmlformat(description), inline=False)
         await bot.send_message(message.channel, embed=msg)
 
